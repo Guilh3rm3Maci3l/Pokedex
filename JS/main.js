0fb8bacba5be2b28df01c2fb789ae9a7adbdd940
Guilh3rm3Maci3l <116690354+Guilh3rm3Maci3l@users.noreply.github.com>
2025-01-24 13:34:24 -0300

    Atualização dinâmica total
    Agora cada detalhe dos pokemons é atualizado (tipo, nome, imagem, número)

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,30 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+document.addEventListener("DOMContentLoaded", () => {
+    const offset = 0;
+    const limit = 200;
 
-function convertpokemontoli(pokemon) {
-      return `
-                <li class="pokemon">
-                <span class="number">#001</span>
+    // Função para converter os dados do Pokémon em HTML
+    function convertpokemontoli(pokemon) {
+        return `
+            <li class="pokemon">
+                <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ul class="types">
-                        <li class="type">Grass</li>
-                        <li class="type">Poison</li>
+                        ${pokemon.types.map((type) => `<li class="type">${type.type.name}</li>`).join('')}
                     </ul>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
-                    alt="${pokemon.name}">
+                    <img src="${pokemon.sprites.other['dream_world'].front_default}" alt="${pokemon.name}">
                 </div>
             </li>
-      `
+        `;
+    }
 
-}
-const pokemonlist = document.getElementById('pokemonlist');
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+    const pokemonlist = document.getElementById('pokemonlist');
 
-        for(let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonlist.innerHTML += convertpokemontoli(pokemon) 
-        }
-    })
-    .catch((error) => console.error(error));
-     
+    // Obtém os Pokémons e renderiza
+    pokeApi.getpokemons(offset, limit).then((pokemons = []) => {
+        const newlist = pokemons.map((pokemon) => convertpokemontoli(pokemon));
+        pokemonlist.innerHTML = newlist.join('');
+    }).catch((error) => {
+        console.error("Erro ao renderizar a lista de Pokémons:", error);
+    });
+});
--- JS/poke-api.js
+++ JS/poke-api.js
@@ -1,9 +1,16 @@
-
-const pokeaApi = {}
-    pokeaApi.getpokemons = (offset = 0, limit = 10) => {
-        const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-        fetch(url)
-            .then((response) => response.json())
-            .then((jsonBody) => jsonBody.results)
-            .catch((error) => console.error(error));
-    }
+const pokeApi = {};
+pokeApi.getpokemons = (offset = 0, limit = 100) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => {
+            const pokemonList = jsonBody.results;
+            // Adiciona os detalhes individuais dos Pokémons
+            return Promise.all(
+                pokemonList.map((pokemon) => fetch(pokemon.url).then((res) => res.json()))
+            );
+        })
+        .catch((error) => {
+            console.error("Erro ao buscar Pokémons:", error);
+        });
+};
